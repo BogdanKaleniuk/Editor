1109f37e795e70efb47a7345e24f40638174b977
Bogdan Kaleniuk <bogdankaleniuk95@gmail.com>
2024-03-14 22:58:47 +0200

    Updates

--- src/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-const queryClient = new QueryClient();
-
-export function Provider({ children }: PropsWithChildren) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-}
